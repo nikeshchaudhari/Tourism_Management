4b70e7e26c556648aad90c561c3e13cc702a5c76
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-01 19:04:42 +0545

    Final Project

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+
+
+
+
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="#"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="#"><span>Twitter</span></a></li>
+				
+		</div>
+		<p class="" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; "> Tourism Management System . All Rights Reserved </p>
+	</div>
+</div>
